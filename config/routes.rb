8bdb9c653bb922c3e8d8536754ddfc9ca4d85300
Alfredo Gallegos <alfredo.gallegos@naranya.com>
2015-11-22 21:17:46 -0600

    adds boilerplate Say controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   get 'say/goodbye'
 
+  get 'say/hello'
+
+  get 'say/goodbye'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
